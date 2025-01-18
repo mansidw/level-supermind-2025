a623b60bc3df3dc85be142cf69fb35ed9e81274d
Kushal Shah <shahkushal38@gmail.com>
2025-01-18 23:45:21 +0530

    Db connect

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,14 @@ from flask import Flask
 import os
 from flask_cors import CORS
 from dotenv import load_dotenv
+from db import initialize_client
 
 load_dotenv()
 app = Flask(__name__)
 CORS(app)
+
+db = initialize_client()
+print(f"Connected to Astra DB: {db.list_collection_names()}")
 
 
 @app.route("/health")
---
+++ backend/db.py
@@ -0,0 +1,23 @@
+from astrapy import DataAPIClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize the client
+def initialize_client():
+    try:
+        token = os.getenv("ASTRA_DB_TOKEN")
+        endpoint = os.getenv("ASTRA_DB_ENDPOINT")
+        
+        if not token or not endpoint:
+            raise ValueError("AstraDB token or endpoint not found in environment variables.")
+        
+        client = DataAPIClient(token)
+        print('CLient - ',client)
+        db = client.get_database_by_api_endpoint(endpoint)
+        return db
+    except Exception as e:
+        print(f"Error initializing AstraDB client: {e}")
+        return None
+
